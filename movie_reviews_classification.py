c4f5ff966035e4b0a5082915df05b798bc4ea4c7
gamraniyoussef <gamraniyoussef@gmail.com>
2020-01-06 16:06:50 +0100

    init

---
+++ movie_reviews_classification.py
@@ -0,0 +1,54 @@
+from keras.datasets import imdb
+import numpy as np
+from tensorflow.keras import models
+from tensorflow.keras import layers
+
+# save np.load
+np_load_old = np.load
+
+# modify the default parameters of np.load
+np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+np.load = np_load_old
+
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1.
+    return results
+
+x_train = vectorize_sequences(train_data)
+x_test = vectorize_sequences(test_data)
+y_train = np.asarray(train_labels).astype('float32')
+y_test = np.asarray(test_labels).astype('float32')
+
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+x_val = x_train[:10000]
+partial_x_train = x_train[10000:]
+y_val = y_train[:10000]
+partial_y_train = y_train[10000:]
+
+history = model.fit(partial_x_train,
+                   partial_y_train,
+                   epochs=20,
+                   batch_size=512,
+                   validation_data=(x_val, y_val))
+
+history_dict = history.history
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+val_accuracy = history_dict['val_accuracy']
+epochs = range(1, len(val_accuracy) + 1)
+plt.plot(epochs, loss_values, 'bo', label='Training loss')
+plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+model.predict(x_test)
---
+++ naive_relu.py
@@ -0,0 +1,18 @@
+def naive_relu(x):
+    assert len(x.shape) == 2
+
+    x = x.copy()
+    for i in range(x.shape[0]):
+        for j in range(x.shape[1]):
+            x[i, j] = max(x[i, j], 0)
+    return  x
+
+def naive_add(x, y):
+    assert len(x.shape) == 2
+    assert x.shape == y.shape
+
+    x = x.copy()
+    for i in range(x.shape[0]): 
+        for j in range(x.shape[1]): 
+            x[i,j] += y[i,j]
+    return x
